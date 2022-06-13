180c07a809f3e47206a271e6d236141386978ae2
김현우 <88073093+khw12326@users.noreply.github.com>
2022-06-13 14:33:45 +0900

    Add files via upload

---
+++ TEST01.java
@@ -0,0 +1,15 @@
+
+public class TEST01 {
+
+	public static void main(String[] args) {
+		int[]a = {10,20,30};
+		a[0]=30;
+		a[1]=20;
+		a[2]=10;
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+
+	}
+
+}
